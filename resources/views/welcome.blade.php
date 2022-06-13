956cc66c751544874d85bcef98f0a657e337b09e
Ayush Barasani <ayush.barasani@gmail.com>
2022-06-13 09:26:32 +0545

    Homepage redesign

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,56 +1,208 @@
-@extends('layouts.app')
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <title>Job Finder &mdash; Colorlib Website Template</title>
 
-@section('content')
-<div class="container">
-    <div class="row">
-        <div class="col-md-12">
-            <search></search>
-        </div>
-        <h1>Recent Jobs</h1>
-        <table class="table my-5">
-            <thead>
-                <th>Logo</th>
-                <th>Position</th>
-                <th>Address</th>
-                <th>Posted on</th>
-                <th>Action</th>
-            </thead>
-            <tbody>
-                @foreach($jobs as $job)
-                <tr>
-                    <td><img src="{{asset('uploads/logo')}}/{{$job->company->logo}}" height="60px" width="60px" alt="" srcset=""></td>
-                    <td>{{$job->position}}<br>
-                    <i class="fa fa-clock" aria-hidden="true">&nbsp;{{$job->type}}</i>
-                    </td>
-                    <td><i class="fa fa-map-marker" aria-hidden="true"></i> {{$job->address}}</td>
-                    <td><i class="fa fa-globe" aria-hidden="true">{{$job->created_at->diffForHumans()}}</td>
-                    <td><a href="{{route('jobs.show', [$job->id,$job->slug])}}"><button class="btn btn-success">View</button></a></td>
-                </tr>
-                @endforeach
-            </tbody>
-        </table>
-    </div>
-    <div>
-        <a href="{{route('alljobs')}}"><button class="btn btn-secondary btn-lg" style="width: 100%;">Browse all jobs</button></a><br><br>
-        <h3 style="display: flex; justify-content:center;">Featured Companies</h3>
-    </div>
-</div>
+    @include('partials.header');
+    
+  </head>
+  <body>
 
-<div class="container">
-    <div class="row">
-        @foreach($companies as $company)
-        <div class="col-md-3">
-        <div class="card" style="width: 18rem;">
-            <img src="{{asset('uploads/logo')}}/{{$company->logo}}" class="card-img-top" style="height: 200px; width: 18rem;" alt="...">
-                <div class="card-body">
-                    <h5 class="card-title">{{$company->name}}</h5>
-                    <p class="card-text">{{$company->address}}</p>
-                    <p class="card-text">{{$company->website}}</p>
-                    <a href="{{route('company.index',[$company->id,$company->name])}}" class="btn btn-primary">View Company</a>
+  @include('partials.nav');
+
+  @include('partials.hero');
+
+  @include('partials.category');
+
+
+    <div class="site-section bg-light">
+      <div class="container">
+        <div class="row">
+          <div class="col-md-12 mb-5 mb-md-0" data-aos="fade-up" data-aos-delay="100">
+            <h2 class="mb-5 h3">Recent Jobs</h2>
+            <div class="rounded border jobs-wrap">
+
+              <a href="job-single.html" class="job-item d-block d-md-flex align-items-center  border-bottom fulltime">
+                <div class="company-logo blank-logo text-center text-md-left pl-3">
+                  <img src="external/images/company_logo_blank.png" alt="Image" class="img-fluid mx-auto">
                 </div>
+                <div class="job-details h-100">
+                  <div class="p-3 align-self-center">
+                    <h3>Restaurant Crew</h3>
+                    <div class="d-block d-lg-flex">
+                      <div class="mr-3"><span class="icon-suitcase mr-1"></span> Resto Bar</div>
+                      <div class="mr-3"><span class="icon-room mr-1"></span> Florida</div>
+                      <div><span class="icon-money mr-1"></span> $55000 &mdash; 70000</div>
+                    </div>
+                  </div>
+                </div>
+                <div class="job-category align-self-center">
+                  <div class="p-3">
+                    <span class="text-info p-2 rounded border border-info">Full Time</span>
+                  </div>
+                </div>  
+              </a>
+
+              <a href="job-single.html" class="job-item d-block d-md-flex align-items-center freelance">
+                <div class="company-logo blank-logo text-center text-md-left pl-3">
+                  <img src="external/images/logo_1.png" alt="Image" class="img-fluid mx-auto">
+                </div>
+                <div class="job-details h-100">
+                  <div class="p-3 align-self-center">
+                    <h3>JavaScript Fullstack Developer</h3>
+                    <div class="d-block d-lg-flex">
+                      <div class="mr-3"><span class="icon-suitcase mr-1"></span> Cooper</div>
+                      <div class="mr-3"><span class="icon-room mr-1"></span> Anywhere</div>
+                      <div><span class="icon-money mr-1"></span> $55000 &mdash; 70000</div>
+                    </div>
+                  </div>
+                </div>
+                <div class="job-category align-self-center">
+                  <div class="p-3">
+                    <span class="text-warning p-2 rounded border border-warning">Freelance</span>
+                  </div>
+                </div>  
+              </a>
+
+
+              <a href="job-single.html" class="job-item d-block d-md-flex align-items-center freelance">
+                <div class="company-logo blank-logo text-center text-md-left pl-3">
+                  <img src="external/images/logo_1.png" alt="Image" class="img-fluid mx-auto">
+                </div>
+                <div class="job-details h-100">
+                  <div class="p-3 align-self-center">
+                    <h3>ReactJS Fullstack Developer</h3>
+                    <div class="d-block d-lg-flex">
+                      <div class="mr-3"><span class="icon-suitcase mr-1"></span> Cooper</div>
+                      <div class="mr-3"><span class="icon-room mr-1"></span> Anywhere</div>
+                      <div><span class="icon-money mr-1"></span> $55000 &mdash; 70000</div>
+                    </div>
+                  </div>
+                </div>
+                <div class="job-category align-self-center">
+                  <div class="p-3">
+                    <span class="text-warning p-2 rounded border border-warning">Freelance</span>
+                  </div>
+                </div>  
+              </a>
+
+
+              <a href="job-single.html" class="job-item d-block d-md-flex align-items-center fulltime">
+                <div class="company-logo blank-logo text-center text-md-left pl-3">
+                  <img src="external/images/company_logo_blank.png" alt="Image" class="img-fluid mx-auto">
+                </div>
+                <div class="job-details h-100">
+                  <div class="p-3 align-self-center">
+                    <h3>Assistant Brooker, Real Estate</h3>
+                    <div class="d-block d-lg-flex">
+                      <div class="mr-3"><span class="icon-suitcase mr-1"></span> RealState</div>
+                      <div class="mr-3"><span class="icon-room mr-1"></span> New York</div>
+                      <div><span class="icon-money mr-1"></span> $55000 &mdash; 70000</div>
+                    </div>
+                  </div>
+                </div>
+                <div class="job-category align-self-center">
+                  <div class="p-3">
+                    <span class="text-info p-2 rounded border border-info">Full Time</span>
+                  </div>
+                </div>  
+              </a>
+
+              <a href="job-single.html" class="job-item d-block d-md-flex align-items-center partime">
+                <div class="company-logo blank-logo text-center text-md-left pl-3">
+                  <img src="external/images/logo_2.png" alt="Image" class="img-fluid mx-auto">
+                </div>
+                <div class="job-details h-100">
+                  <div class="p-3 align-self-center">
+                    <h3>Telecommunication Manager</h3>
+                    <div class="d-block d-lg-flex">
+                      <div class="mr-3"><span class="icon-suitcase mr-1"></span> Think</div>
+                      <div class="mr-3"><span class="icon-room mr-1"></span> London</div>
+                    </div>
+                  </div>
+                </div>
+                <div class="job-category align-self-center">
+                  <div class="p-3">
+                    <span class="text-danger p-2 rounded border border-danger">Par Time</span>
+                  </div>
+                </div>  
+              </a>
+
+
             </div>
+
+            <div class="col-md-12 text-center mt-5">
+              <a href="#" class="btn btn-primary rounded py-3 px-5"><span class="icon-plus-circle"></span> Show More Jobs</a>
+            </div>
+          </div>
         </div>
-        @endforeach
+      </div>
     </div>
-</div>
-@endsection
+
+    @include('partials.testimonial');
+
+
+    <div class="site-blocks-cover overlay inner-page" style="background-image: url('images/hero_1.jpg');" data-aos="fade" data-stellar-background-ratio="0.5">
+      <div class="container">
+        <div class="row align-items-center justify-content-center">
+          <div class="col-md-6 text-center" data-aos="fade">
+            <h1 class="h3 mb-0">Your Dream Job</h1>
+            <p class="h3 text-white mb-5">Is Waiting For You</p>
+            <p><a href="#" class="btn btn-outline-warning py-3 px-4">Find Jobs</a> <a href="#" class="btn btn-warning py-3 px-4">Apply For A Job</a></p>
+            
+          </div>
+        </div>
+      </div>
+    </div>
+
+    
+
+    <div class="site-section site-block-feature bg-light">
+      <div class="container">
+        
+        <div class="text-center mb-5 section-heading">
+          <h2>Why Choose Us</h2>
+        </div>
+
+        <div class="d-block d-md-flex border-bottom">
+          <div class="text-center p-4 item border-right" data-aos="fade">
+            <span class="flaticon-worker display-3 mb-3 d-block text-primary"></span>
+            <h2 class="h4">More Jobs Every Day</h2>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Obcaecati reprehenderit explicabo quos fugit vitae dolorum.</p>
+            <p><a href="#">Read More <span class="icon-arrow-right small"></span></a></p>
+          </div>
+          <div class="text-center p-4 item" data-aos="fade">
+            <span class="flaticon-wrench display-3 mb-3 d-block text-primary"></span>
+            <h2 class="h4">Creative Jobs</h2>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Obcaecati reprehenderit explicabo quos fugit vitae dolorum.</p>
+            <p><a href="#">Read More <span class="icon-arrow-right small"></span></a></p>
+          </div>
+        </div>
+        <div class="d-block d-md-flex">
+          <div class="text-center p-4 item border-right" data-aos="fade">
+            <span class="flaticon-stethoscope display-3 mb-3 d-block text-primary"></span>
+            <h2 class="h4">Healthcare</h2>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Obcaecati reprehenderit explicabo quos fugit vitae dolorum.</p>
+            <p><a href="#">Read More <span class="icon-arrow-right small"></span></a></p>
+          </div>
+          <div class="text-center p-4 item" data-aos="fade">
+            <span class="flaticon-calculator display-3 mb-3 d-block text-primary"></span>
+            <h2 class="h4">Finance &amp; Accounting</h2>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Obcaecati reprehenderit explicabo quos fugit vitae dolorum.</p>
+            <p><a href="#">Read More <span class="icon-arrow-right small"></span></a></p>
+          </div>
+        </div>
+      </div>
+    </div>
+
+    
+
+
+    @include('partials.blog');
+    
+
+
+    
+    @include('partials.footer');
+  </body>
+</html>
